c1340f0a389af758fe5939b606a7dc616c68d04d
Gus <guscomps@gmail.com>
2019-10-08 22:37:28 -0400

    Add docstrings

--- tests/python/unittest/test_custom_datatypes_change_dtype.py
+++ tests/python/unittest/test_custom_datatypes_change_dtype.py
@@ -173,12 +173,14 @@ def setup():
 
 
 def convert_ndarray(dst_dtype, array, executor):
+    """Converts an NDArray into the specified datatype"""
     x = relay.var('x', shape=array.shape, dtype=str(array.dtype))
     cast = relay.Function([x], x.astype(dst_dtype))
     return executor.evaluate(cast)(array)
 
 
 def change_dtype(src, dst, expr, params, executor):
+    """Change the datatype of a relay expression"""
     # TODO(gus) There's probably a better way to do this---update cdtype to work
     # over modules
     expr = transform.InferType()(relay.Module.from_expr(expr))
